1d22e051ef6290aa3023a6d7a16a20ed5f64eac1
Leo Mikko Uson <usonleomikko@gmail.com>
2024-08-27 15:17:21 +0800

    Add TypeScript interface for RadioButton component props

--- components/RadioButton.tsx
+++ components/RadioButton.tsx
@@ -1,7 +1,19 @@
 import { Label } from "@/components/ui/label";
 import { RadioGroupItem } from "@/components/ui/radio-group";
 
-export function RadioButton({ value, id, children, ...props }) {
+interface RadioButtonProps {
+  value: string;
+  id: string;
+  children: React.ReactNode;
+  [key: string]: any;
+}
+
+export function RadioButton({
+  value,
+  id,
+  children,
+  ...props
+}: RadioButtonProps) {
   return (
     <div className="flex items-center space-x-2">
       <RadioGroupItem value={value} id={id} {...props} />
