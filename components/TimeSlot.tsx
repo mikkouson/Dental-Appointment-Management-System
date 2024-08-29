514b1e5ea00303b2fb2d0e0abdd59c1270edde40
Leo Mikko Uson <usonleomikko@gmail.com>
2024-08-29 13:27:58 +0800

    Refactor `TimeSlot` component to derive `date` from `selectedDate` in `useGetDate` hook and add null check for `data` before mapping.

--- components/TimeSlot.tsx
+++ components/TimeSlot.tsx
@@ -11,8 +11,9 @@ interface TimeSlotProps {
   date: string;
 }
 
-export default function TimeSlot({ date }: TimeSlotProps) {
-  const { getTime, selectedTime } = useGetDate();
+export default function TimeSlot() {
+  const { getTime, selectedTime, selectedDate } = useGetDate();
+  const date = moment(selectedDate).format("MM/DD/YYYY");
   const { data, error } = useSWR(`/api/timeslots?date=${date}`, fetcher);
 
   if (error) return <div>Error loading timeslots.</div>;
@@ -30,27 +31,28 @@ export default function TimeSlot({ date }: TimeSlotProps) {
       }}
       className="flex flex-wrap"
     >
-      {data.map((slot: { id: string; time: string; appointments: any[] }) => {
-        const isDisabled = slot.appointments.length >= 3;
-        return (
-          <ToggleGroupItem
-            key={slot.id}
-            value={slot.id}
-            aria-disabled={isDisabled}
-            className={`${isDisabled ? "bg-gray-300 cursor-not-allowed" : ""} ${
-              selectedTime === slot.id ? "bg-gray-200" : ""
-            }`}
-            disabled={isDisabled}
-          >
-            <div className="flex ">
-              <span className="mr-4">
-                Time: {moment(slot.time, "HH:mm").format("hh:mm A")}
-              </span>
-              <span>Slot: {slot.appointments.length}</span>
-            </div>
-          </ToggleGroupItem>
-        );
-      })}
+      {data &&
+        data?.map((slot: { id: string; time: string; appointments: any[] }) => {
+          const isDisabled = slot.appointments.length >= 3;
+          return (
+            <ToggleGroupItem
+              key={slot.id}
+              value={slot.id}
+              aria-disabled={isDisabled}
+              className={`${
+                isDisabled ? "bg-gray-300 cursor-not-allowed" : ""
+              } ${selectedTime === slot.id ? "bg-gray-200" : ""}`}
+              disabled={isDisabled}
+            >
+              <div className="flex ">
+                <span className="mr-4">
+                  Time: {moment(slot.time, "HH:mm").format("hh:mm A")}
+                </span>
+                <span>Slot: {slot.appointments.length}</span>
+              </div>
+            </ToggleGroupItem>
+          );
+        })}
     </ToggleGroup>
   );
 }
