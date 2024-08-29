eae75b9071b0d2812c6be75d60be5a38f69192ea
Leo Mikko Uson <usonleomikko@gmail.com>
2024-08-29 15:25:49 +0800

    Add Inventory page to admin section with data fetching using SWR

---
+++ app/admin/inventory/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Item } from "@radix-ui/react-dropdown-menu";
+import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+const Inventory = () => {
+  const { data, error } = useSWR(`/api/inventory`, fetcher);
+  if (error) return <div>Error loading timeslots.</div>;
+  if (!data) return <div>Loading...</div>;
+  return (
+    <>
+      {data.map((item) => (
+        <div>
+          <p>{item.id}</p>
+          <p>{item.name}</p>
+          <p>{item.description}</p>
+          <p>{item.quantity}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Inventory;
