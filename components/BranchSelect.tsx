8bf77deff50ddb482e8806f77e4f35b62fd08574
Leo Mikko Uson <usonleomikko@gmail.com>
2024-09-01 14:00:20 +0800

    Refactor `BranchSelect` component to use new `RadioComboBox` component for dropdown functionality.

--- components/BranchSelect.tsx
+++ components/BranchSelect.tsx
@@ -2,18 +2,9 @@
 
 import { useGetDate } from "@/app/store";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { ChevronDownIcon } from "lucide-react";
-import React from "react";
 import useSWR from "swr";
+import RadioComboBox from "./RadioComboBox";
 
 const fetcher = (url: string): Promise<any[]> =>
   fetch(url).then((res) => res.json());
@@ -27,7 +18,7 @@ export default function BranchSelect() {
   if (error) return <>Failed to load</>;
   if (!data)
     return (
-      <Button variant="outline">
+      <Button variant="outline" disabled>
         Branch : Loading ...
         <ChevronDownIcon className="ml-2 h-4 w-4" />
       </Button>
@@ -37,31 +28,13 @@ export default function BranchSelect() {
 
   return (
     <div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline">
-            Branch : {selectedBranch ? selectedBranch.name : "Select a branch"}
-            <ChevronDownIcon className="ml-2 h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56">
-          <DropdownMenuLabel>Appearance</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {data.map((branch) => (
-            <DropdownMenuRadioGroup
-              key={branch.id}
-              value={branch1.toString()}
-              onValueChange={(value) => {
-                setBranch(Number(value));
-              }}
-            >
-              <DropdownMenuRadioItem value={branch.id.toString()}>
-                {branch.name}
-              </DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <RadioComboBox
+        data={data}
+        selectedItem={selectedBranch}
+        store={branch1}
+        action={setBranch}
+        label={"Branch"}
+      />
     </div>
   );
 }
