d0cbe23b2b0a0fe217aeae5450103c011ca0a115
Leo Mikko Uson <usonleomikko@gmail.com>
2024-09-08 16:56:49 +0800

    fix: Update submitBtn component to handle optional formAction prop

--- components/submitBtn.tsx
+++ components/submitBtn.tsx
@@ -13,7 +13,7 @@ export default function SubmitButton({
 }: Props) {
   const { pending, action } = useFormStatus();
 
-  const isPending = pending && action == props.formAction;
+  const isPending = pending && action == props?.formAction;
   return (
     <Button {...props} type="submit" aria-disabled={pending}>
       {isPending ? pendingText : children}
