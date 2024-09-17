2070802f475e0a0ec5fac52aba10cfad15010db6
Leo Mikko Uson <usonleomikko@gmail.com>
2024-09-17 22:36:14 +0800

    Refactor SubmitButton component to display a loading spinner when pending

--- components/forms/newPatientForm.tsx
+++ components/forms/newPatientForm.tsx
@@ -6,50 +6,100 @@ import { z } from "zod";
 
 import { toast } from "@/components/hooks/use-toast";
 import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
-import FormField from "./formField";
 import Maps from "../gmaps";
+import Field from "./formField";
+import { newPatient } from "@/app/admin/action";
 
 const FormSchema = z.object({
-  name: z.number({
-    required_error: "Please select an email to display.",
+  name: z.string().min(1, { message: "Name is required." }),
+  email: z.string().email().min(1, {
+    message: "Invalid email address.",
   }),
-  email: z.string().email(),
-  address: z.date({
-    required_error: "A date of birth is required.",
-  }),
-  sex: z.number({
-    required_error: "A date of birth is required.",
-  }),
-  age: z.string({
-    required_error: "A date of birth is required.",
+  address: z
+    .object({
+      address: z.string({
+        required_error: "Address is required.",
+      }),
+      latitude: z.number().optional(),
+      longitude: z.number().optional(),
+    })
+    .refine((data) => data.address.trim().length > 0, {
+      message: "Address must be provided.",
+    }),
+  sex: z.string().min(1, { message: "Sex is required." }),
+  age: z.number().min(1, {
+    message: "Age is required.",
   }),
 });
+const sex = [
+  { name: "Male", id: "male" },
+  { name: "Female", id: "female" },
+  { name: "Prefer not to say", id: "prefer_not_to_say" },
+] as const;
 
 export function SelectForm({ setOpen }: { setOpen: (open: boolean) => void }) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      sex: "",
+      age: 0,
+    },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    setOpen(false);
+    // Ensure `age` is correctly typed as a number
+    const correctedData = {
+      ...data,
+      age: Number(data.age),
+    };
+
+    // setOpen(false);
+    newPatient(correctedData);
     toast({
       title: "You submitted the following values:",
       description: (
         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          <code className="text-white">
+            {JSON.stringify(correctedData, null, 2)}
+          </code>
         </pre>
       ),
     });
   }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8">
-          <FormField form={form} name={"name"} label={"Name"} />
-          <FormField form={form} name={"name"} label={"Name"} />
-          <Maps />
+          <Field form={form} name={"name"} label={"Name"} />
+          <Field form={form} name={"email"} label={"Email"} />
+          <Field form={form} data={sex} name={"sex"} label={"Sex"} />
+          <Field form={form} name={"age"} label={"Age"} age={true} />
+
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Maps field={field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="flex justify-end">
           <Button type="submit">Submit</Button>
