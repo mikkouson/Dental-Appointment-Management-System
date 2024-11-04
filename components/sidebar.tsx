5d83313aa9bd73472423dea5b36cda7f95c0f4a9
Leo Mikko Uson <usonleomikko@gmail.com>
2024-11-04 17:14:58 +0800

    feat: implement theme toggle using switch component and add user navigation menu

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import React, { ReactNode, useState } from "react";
-import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
+import Logo from "@/images/logo.png";
+import { cn } from "@/lib/utils";
 import {
-  IconArrowLeft,
   IconBrandTabler,
   IconSettings,
   IconUserBolt,
-} from "@tabler/icons-react"; // Assuming ChevronLeft is part of Tabler Icons
-import Link from "next/link";
+} from "@tabler/icons-react";
 import { motion } from "framer-motion";
-import Image from "next/image";
-import { cn } from "@/lib/utils";
 import { BriefcaseMedical, Calendar, ChevronLeft, Package } from "lucide-react";
-import Logo from "@/images/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ReactNode, useState } from "react";
+import { NavUser } from "./nav-user";
+import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+};
 
 export const links = [
   {
     label: "Dashboard",
-    href: "/",
+    href: "/dashboard",
     icon: (
       <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
     ),
@@ -38,7 +46,6 @@ export const links = [
       <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
     ),
   },
-
   {
     label: "Inventory",
     href: "/inventory",
@@ -54,13 +61,6 @@ export const links = [
     ),
   },
   {
-    label: "Settings",
-    href: "/settings",
-    icon: (
-      <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
-    ),
-  },
-  {
     label: "Users",
     href: "/users",
     icon: (
@@ -68,20 +68,27 @@ export const links = [
     ),
   },
   {
-    label: "Logout",
-    href: "#",
+    label: "Settings",
+    href: "/settings",
     icon: (
-      <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
     ),
   },
 ];
+
 interface Props {
   children?: ReactNode;
-  // any props that come into the component
+  user: any;
 }
-export function SidebarDemo({ children, ...props }: Props) {
+
+export function SidebarDemo({ user, children, ...props }: Props) {
   const [open, setOpen] = useState(true);
   const path = usePathname();
+
+  if (path === "/login") {
+    return <>{children}</>;
+  }
+
   const handleToggle = () => {
     setOpen(!open);
   };
@@ -89,31 +96,21 @@ export function SidebarDemo({ children, ...props }: Props) {
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden ",
-        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`,
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen"
       )}
     >
-      {/* Sidebar component */}
       <Sidebar open={open} setOpen={setOpen}>
-        <SidebarBody className="justify-between gap-10 md:relative ">
-          <div
-            className={cn(
-              "flex flex-col flex-1 overflow-y-auto overflow-x-hidden "
-            )}
-          >
-            {open ? (
-              <Logos /> // Replace with your open state content
-            ) : (
-              <LogoIcon /> // Replace with your closed state content
-            )}
+        <SidebarBody className="justify-between gap-10 md:relative">
+          <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+            {open ? <Logos /> : <LogoIcon />}
             <div className="mt-8 flex flex-col gap-1">
               {links.map((link, idx) => (
                 <div
                   key={idx}
                   className={cn(
-                    "px-2 rounded-lg ",
+                    "px-2 rounded-lg",
                     path === link.href
-                      ? "dark:bg-[#1c1c21] border dark:border-neutral-700 bg-white border-input "
+                      ? "dark:bg-[#1c1c21] border dark:border-neutral-700 bg-white border-input"
                       : "transparent",
                     "opacity-80"
                   )}
@@ -123,76 +120,58 @@ export function SidebarDemo({ children, ...props }: Props) {
               ))}
             </div>
           </div>
+
           <div>
-            {/* Example of Avatar link */}
-            {/* <SidebarLink
-              link={{
-                label: "Lobodent",
-                href: "#",
-                icon: (
-                  <Image
-                    src={Logo}
-                    className="h-7 w-7 flex-shrink-0 rounded-full"
-                    width={50}
-                    height={50}
-                    alt="Avatar"
-                  />
-                ),
-              }}
-            /> */}
+            <NavUser user={user} />
           </div>
+
           <ChevronLeft
             className={cn(
-              " -right-3 top-10 z-50 cursor-pointer rounded-full border bg-background text-3xl text-foreground absolute ",
+              "-right-3 top-10 z-50 cursor-pointer rounded-full border bg-background text-3xl text-foreground absolute",
               open && "rotate-180"
             )}
-            onClick={handleToggle} // Toggle sidebar open/close on click
+            onClick={handleToggle}
           />
         </SidebarBody>
       </Sidebar>
-      {/* Placeholder for Dashboard component */}
-      {/* <Dashboard /> */}
       {children}
     </div>
   );
 }
 
-export const Logos = () => {
-  return (
-    <Link
-      href="/"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+export const Logos = () => (
+  <Link
+    href="/"
+    className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+  >
+    <Image
+      src={Logo}
+      className="h-7 w-7 flex-shrink-0 rounded-full"
+      width={50}
+      height={50}
+      alt="Avatar"
+    />
+    <motion.span
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="font-medium text-black dark:text-white whitespace-pre"
     >
-      <Image
-        src={Logo}
-        className="h-7 w-7 flex-shrink-0 rounded-full"
-        width={50}
-        height={50}
-        alt="Avatar"
-      />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className="font-medium text-black dark:text-white whitespace-pre"
-      >
-        Lobodent Dental
-      </motion.span>
-    </Link>
-  );
-};
-export const LogoIcon = () => {
-  return (
-    <Link
-      href="/"
-      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
-    >
-      <Image
-        src={Logo}
-        className="h-7 w-7 flex-shrink-0 rounded-full"
-        width={50}
-        height={50}
-        alt="Avatar"
-      />
-    </Link>
-  );
-};
+      Lobodent Dental
+    </motion.span>
+  </Link>
+);
+
+export const LogoIcon = () => (
+  <Link
+    href="/"
+    className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+  >
+    <Image
+      src={Logo}
+      className="h-7 w-7 flex-shrink-0 rounded-full"
+      width={50}
+      height={50}
+      alt="Avatar"
+    />
+  </Link>
+);
